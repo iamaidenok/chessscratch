97aed4851114c59d599ead004b4e89817f9de911
iamaidenok <okaiden25@gmail.com>
2026-05-03 14:45:28 -0400

    p sure im done will double check later

--- classincluded.py
+++ classincluded.py
@@ -78,6 +78,38 @@ class Pawn: #en passant might lowk fuck my crack
             square = board.opp_translating_grid(row,column)
             legal_moves_translated.append(square)
         return legal_moves_translated
-pawn = Pawn('B')
-moves = pawn.move_collector(board,'a7')
-print(moves)
+class Game: 
+    #should have turns, ask player for moves, check if move is legal, special rules, 
+    def __init__(self):
+        self.board = Board()
+        self.turn = 'W'
+
+    def determine_turn(self):
+        if self.turn == 'W':
+            self.turn = 'B'
+        else: 
+            self.turn = 'W'
+    
+    def ask_player_for_moves(self):
+        self.board.print_board()
+        start = input("Pick a piece: \n")
+        end = input("Where you moving it? \n")
+        piece = self.board.get_piece(start)
+        
+        if piece[0] != self.turn:
+            print("pick ur own piece fn")
+            return
+        
+        #pawn
+        if piece[1] == "P":
+            pawn = Pawn(self.turn)
+            legal_moves = pawn.move_collector(self.board,start)
+            if end in legal_moves:
+                self.board.move_piece(start,end)
+                self.determine_turn()
+            else:
+                print('pick a legal move fn')
+
+game = Game()
+while True:
+    game.ask_player_for_moves()
